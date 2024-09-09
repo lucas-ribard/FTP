3ce70e221232b086ea6aa37f87db16bcf1a45bc1
lucas-ribard <lucas.ribard@laplateforme.io>
2024-09-09 12:06:49 +0200

    Update client and server to use network_utils

--- client.cpp
+++ client.cpp
@@ -1,40 +1,22 @@
+#include "network_utils.hpp"
 #include <iostream>
-#include <cstring>
-#include <sys/socket.h>
-#include <arpa/inet.h>
 #include <unistd.h>
 
 #define PORT 8080
 
 int main() {
-    int sock = 0;
-    struct sockaddr_in serv_addr;
+    int sock = create_socket();
+
+    connect_to_server(sock, "127.0.0.1", PORT);
+
     const char *ping = "ping";
-    char buffer[1024] = {0};
-
-    // socket creation
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        std::cerr << "Échec de la création du socket" << std::endl;
-        return -1;
-    }
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    // address conversion
-    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-        std::cerr << "Adresse invalide" << std::endl;
-        return -1;
-    }
-
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        std::cerr << "Échec de la connexion" << std::endl;
-        return -1;
-    }
-    // read write
-    send(sock, ping, strlen(ping), 0);
+    send_message(sock, ping);
     std::cout << "Message envoyé : ping" << std::endl;
-    read(sock, buffer, 1024);
+
+    char buffer[1024] = {0};
+    receive_message(sock, buffer, 1024);
     std::cout << "Réponse reçue : " << buffer << std::endl;
+
     close(sock);
     return 0;
 }

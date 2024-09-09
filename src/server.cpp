a29d070449471d9a308d015a994fd1e1747db9fb
lucas-ribard <lucas.ribard@laplateforme.io>
2024-09-09 15:32:29 +0200

    handle invalid messages

--- src/server.cpp
+++ src/server.cpp
@@ -1,10 +1,12 @@
 #include "network_utils.hpp"
 #include <iostream>
 #include <unistd.h>
+#include <string.h>
 
 #define PORT 8080
 
-int main() {
+int main()
+{
     int server_fd = create_socket();
     struct sockaddr_in address;
 
@@ -12,16 +14,26 @@ int main() {
     listen_socket(server_fd);
 
     std::cout << "En attente de connexions..." << std::endl;
-    
+
     int new_socket = accept_connection(server_fd, address);
-    
+
     char buffer[1024] = {0};
     receive_message(new_socket, buffer, 1024);
     std::cout << "Message reçu : " << buffer << std::endl;
 
-    const char *pong = "pong";
-    send_message(new_socket, pong);
-    std::cout << "Réponse envoyée : pong" << std::endl;
+    if (strcmp(buffer, "ping") != 0){
+        std::cerr << "Message invalide, le server attend 'ping' " << std::endl;
+
+        close(new_socket);
+        close(server_fd);
+
+        exit(EXIT_FAILURE);
+    }
+    else{
+        const char *pong = "pong";
+        send_message(new_socket, pong);
+        std::cout << "Réponse envoyée : pong" << std::endl;
+    }
 
     close(new_socket);
     close(server_fd);
